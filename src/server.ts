93735495cbebc93ca35366fd91c1a8f16af4be88
rom guyer <romguyer@roms-MacBook-Pro.local>
2023-10-01 21:58:01 +0300

    changed cors options

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,9 @@ import { createServer } from 'node:http';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*", // TODO: Change this to your domain name when you deploy the project!
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
