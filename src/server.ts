2214465886e25a6ca57c85edc789eb3b12d276a9
rom guyer <romguyer@roms-MacBook-Pro.local>
2023-10-01 22:00:37 +0300

    chanegd cors again

--- src/server.ts
+++ src/server.ts
@@ -11,7 +11,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "*", // TODO: Change this to your domain name when you deploy the project!
+  origin: "https://main--visionary-rabanadas-fbd1e6.netlify.app", // TODO: Change this to your domain name when you deploy the project!
 }));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
