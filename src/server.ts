7bb90334fea26aeba498b5316ec75745b0154c86
rom guyer <romguyer@roms-MacBook-Pro.local>
2023-10-02 10:02:22 +0300

    fixed test route

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,7 @@ app.use(cors({
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-app.get('/test', (res: Response) => {
+app.get('/test', (req: Request, res: Response) => {
   try {
     res.status(202).json('server is running you can relax')
   } catch (error) {
